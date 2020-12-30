c1cfac7e56554b602adf4c6740aef443d3dcedf3
satyainturi <satyainturi07@gmail.com>
2020-12-30 22:40:10 +0100

    calcLib.c

--- calcLib.c
+++ calcLib.c
@@ -1,73 +1,71 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include <time.h>
-
-/* Here we use " as the calcLib.c and calcLib.h files are in the same folder, and are to be BUILT
-   to into a library, that will be included in other files. 
-
-   This is a C lib, and will be built as such.
-   
-*/ 
 #include "calcLib.h"
 
+#define RANDOM_MAX_VALUE 100000
 
-/* array of char* that points to char arrays.  */ 
-char *arith[]={"add","div","mul","fsub","fadd","fdiv","fmul","fsub"};
+std::string arith[8] = {"add", "div", "mul", "fsub", "fadd", "fdiv", "fmul", "fsub"};
 
-/* Used for random number */
-time_t myData_seedValue;
 
-int initCalcLib(void){
-  /* Init the random number generator with a seed, based on the current time--> should be randomish each time called */
-  srand((unsigned) time(&myData_seedValue));
-  return(0);
+int random_int(void) 
+{
+  return rand() % RANDOM_MAX_VALUE;
+};
+
+double random_double(void) 
+{
+  return (double)(random_int() / 3.0);
+};
+
+void create_calc_data(struct calc_data* calc) 
+{
+  int randId = rand() % 8;
+  
+  calc->str = arith[randId];
+  calc->val1 = random_int();
+  calc->val2 = random_int();
+  calc->fval1 = random_double();
+  calc->fval2 = random_double();
 }
 
-int initCalcLib_seed(unsigned int seed){
-  /* 
-     Init the random number generator with a FIXED seed, will allow us to grab random numbers 
-     in the same sequence all the time. Good when debugging, bad when running live. 
-
-     This is 'messy' for more details see https://en.wikipedia.org/wiki/Pseudorandom_number_generator. 
-
-     DO NOT USE rand() for production, wher you NEED good random numbers. 
-  */
-  
-  myData_seedValue=seed;
-  srand(seed);
-  return(0);
+void get_calc_data_string(struct calc_data* calc, char* text) 
+{
+  if (calc->str.size() == 3) {
+    sprintf(text, "%s %d %d\n", calc->str.c_str(), calc->val1, calc->val2);
+  }
+  else {
+    sprintf(text, "%s %.8g %.8g\n", calc->str.c_str(), calc->fval1, calc->fval2);
+  }
 }
-  
-char *randomType(void){
-  int Listitems=sizeof(arith)/(sizeof(char*)); 
-  /* Figure out HOW many entries there are in the list.
-     First we get the total size that the array of pointers use, sizeof(arith). Then we divide with 
-     the size of a pointer (sizeof(char*)), this gives us the number of pointers in the list. 
-  */
-  int itemPos=rand() % Listitems;
-  /* As we know the number of items, we can just draw a random number and modulo it with the number 
-     of items in the list, then we will get a random number between 0 and the number of items in the list 
-     
-     Using that information, we just return the string found at that position arith[itemPos];
-  */
-  return(arith[itemPos]);
-  
-};
 
-
-int randomInt(void){
-  /* Draw a random interger between o and RAND_MAX, then modulo this with 100 to get a random 
-     number between 0 and 100. */
-  
-  return( rand()%100 );
-};
-
-
-double randomFloat(void){
-  /* The same as for the interber, but for a double, and without the modulo. We cant use 
-     the module approach as it would generate integers, which we do not want. */
-  double x = (double)rand()/(double)(RAND_MAX/100.0);
-  return(x);
-};
+double calculate(struct calc_data* calc) 
+{
+  if (calc->str == "add") {
+    return (double)(calc->val1 + calc->val2);
+  }
+  if (calc->str == "sub") {
+    return (double)(calc->val1 - calc->val2);
+  }
+  if (calc->str == "mul") {
+    return (double)(calc->val1 * calc->val2);
+  }
+  if (calc->str == "div") {
+    return (double)calc->val1 / (double)calc->val2;
+  }
+  if (calc->str == "fadd") {
+    return calc->fval1 + calc->fval2;
+  }
+  if (calc->str == "fsub") {
+    return calc->fval1 - calc->fval2;
+  }
+  if (calc->str == "fmul") {
+    return calc->fval1 * calc->fval2;
+  }
+  if (calc->str == "fdiv") {
+    return calc->fval1 / calc->fval2;
+  }
+  return 0;
+}
 
 
